f8d524a3b11b917629419f389dc8dfe44236ccde
Wiktor Nobis <nobis171@wp.pl>
2022-09-16 19:24:47 +0200

    add nav with hamburger menu

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,39 +1,45 @@
 import { Link } from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheckCircle, faUserShield} from "@fortawesome/free-solid-svg-icons";
+
+import HamburgerMenu from "./HamburgerMenu";
 
 import './_nav.scss';
 
 function Nav() {
     return (
         <nav className="nav">
-            {/* <HamburgerMenu /> */}
             <div className="nav_menu">
                 <div className="nav_menu_first_column">
                     <div className="first_container">
-                        <p className="first_container_icon">1</p>
+                        <p className="first_container_icon"><FontAwesomeIcon icon={faCheckCircle} /></p>
                         <p className="first_container_text">Najwysza jakość</p>
                     </div>
                     <div className="second_container">
-                        <p className="second_container_icon">2</p>
+                        <p className="second_container_icon"><FontAwesomeIcon icon={faUserShield} /></p>
                         <p className="second_container_text">2-letnia gwarancja</p>
                     </div>
                 </div>
-                <ul className="nav_menu_second_column">
-                    <div className="nav_menu_second_column">
+                <div className="nav_menu_second_column">
+                <HamburgerMenu />
+                    <div className="nav_menu_second_column_logo">
                         <p>logo</p>
                     </div>
-                    <li className="nav_menu_second_column_text">
-                        <Link to='/'>Strona główna</Link>
-                    </li>
-                    <li className="nav_menu_second_column_text">
-                        <Link to='/produkty'>Produkty</Link>
-                    </li>
-                    <li className="nav_menu_second_column_text">
-                        <Link to='/kontakt'>Kontakt</Link>
-                    </li>
-                    <li className="nav_menu_second_column_text">
-                        <Link to='/dojazd'>Dojazd</Link>
-                    </li>
-                </ul>
+                    <ul className="nav_menu_second_column_menu">
+                        <li className="nav_menu_second_column_menu_link">
+                            <Link to='/'>Strona główna</Link>
+                        </li>
+                        <li className="nav_menu_second_column_menu_link">
+                            <Link to='/produkty'>Produkty</Link>
+                        </li>
+                        <li className="nav_menu_second_column_menu_link">
+                            <Link to='/kontakt'>Kontakt</Link>
+                        </li>
+                        <li className="nav_menu_second_column_menu_link">
+                            <Link to='/dojazd'>Dojazd</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
