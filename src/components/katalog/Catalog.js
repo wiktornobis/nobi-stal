ba035016d9d67f527e8e39ce4af710f958d435cf
Wiktor Nobis <nobis171@wp.pl>
2022-10-01 11:54:46 +0200

    update section

--- src/components/katalog/Catalog.js
+++ src/components/katalog/Catalog.js
@@ -7,16 +7,18 @@ function Catalog() {
     
     return (
         <section className="catalog general-container">
-            <h2 className="catalog_title">Potrzebujesz sprężyn na specjalne zamówienie?</h2>
-            <h4 className="catalog_text">
-                Zapoznaj się z naszą szeroką gamą produktów. 
-                Kliknij przycisk i pobierz katalog zawierający wszystkie produkty.
-            </h4>
-            <Button className='catalog_btn' variant="outlined" size="small" endIcon={<DownloadIcon />}>
-                <a className='catalog_btn_text' href="okladka.pdf" target="_blank" download="okladka.pdf">
-                Pobierz plik
-                </a>
-            </Button>
+            <div className="catalog_container">
+                <h2 className="catalog_title">Potrzebujesz sprężyn na specjalne zamówienie?</h2>
+                <h4 className="catalog_text">
+                    Zapoznaj się z naszą szeroką gamą produktów. 
+                    Kliknij przycisk i pobierz katalog zawierający wszystkie produkty.
+                </h4>
+                <Button className='catalog_btn' variant="outlined" size="small" endIcon={<DownloadIcon />}>
+                    <a className='catalog_btn_text' href="okladka.pdf" target="_blank" download="okladka.pdf">
+                    Pobierz plik
+                    </a>
+                </Button>
+            </div>
         </section>
     );
 }
