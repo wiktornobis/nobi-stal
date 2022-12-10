2bb11c737d87ea5f16f51cdf87ac8787ec78ca85
Wiktor Nobis <nobis171@wp.pl>
2022-12-10 13:20:26 +0100

    translate subpage products

--- src/components/katalog/Catalog.js
+++ src/components/katalog/Catalog.js
@@ -5,22 +5,21 @@ import Aos from 'aos';
 import 'aos/dist/aos.css';
 import './_catalog.scss';
 import { useEffect } from 'react';
+import { useTranslation} from 'react-i18next';
 
 function Catalog() {
+    const { t } = useTranslation();
     useEffect(() => {
         Aos.init({ duration: 3000 })
     }, [])
     return (
         <section className="catalog general-container" data-aos="fade-up">
             <div className="container">
-                <h2 className="catalog_title">Potrzebujesz sprężyn na specjalne zamówienie?</h2>
-                <h4 className="catalog_text">
-                    Zapoznaj się z naszą szeroką gamą produktów. 
-                    Kliknij przycisk i pobierz katalog zawierający wszystkie produkty.
-                </h4>
+                <h2 className="catalog_title">{t('catalog.title')}</h2>
+                <h4 className="catalog_text">{t('catalog.text1')}</h4>
                 <Button className='catalog_btn' variant="outlined" size="small" endIcon={<DownloadIcon />}>
                     <a className='catalog_btn_text' href="okladka.pdf" target="_blank" download="okladka.pdf">
-                    Pobierz plik
+                        {t('catalog.text2')}
                     </a>
                 </Button>
             </div>
--- src/components/pages/products/Products.js
+++ src/components/pages/products/Products.js
@@ -37,31 +37,15 @@ function Products({products}) {
                     ))}
                 </div>
                 <div className="information">
-                    <h2 className="information_title" data-aos="fade-up">
-                        NOBI-STAL
-                    </h2>
-                    <h3 className="information_text" data-aos="fade-up">
-                        Nobi-stal jest jednym z największych dostawców sprężyn technicznych
-                        dostępnych z magazynu. Dzięki szerokiej gamie produktów wysokiej jakości 
-                        dostarczamy sprężyny szybko i po korzystnej cenie.
-                        Wieloletnia tradycja i międzynarodowy charakter firmy sprawia, 
-                        że rozwiązania Nobi-Stal spełniają wymogi zarówno norm europejskich, 
-                        UIC, rosyjskich GOST, jak i chińskich TB/T. Innowacyjność, kompetencje 
-                        technologiczne i konstrukcyjne, kilkadziesiąt lat doświadczenia i chęć
-                        podążania za stale rosnącymi wymaganiami Klientów stanowią o jakości 
-                        oferowanych produktów i rozwiązań
-                    </h3>
-                    <h3 className="information_text" data-aos="fade-up">
-                        Ciągle poszerzamy naszą ofertę produktową stając się jeszcze lepszym 
-                        partnerem dla naszych Klientów.
-                    </h3>
+                    <h2 className="information_title" data-aos="fade-up">{t('subpage.productsInformation.title')} </h2>
+                    <h3 className="information_text" data-aos="fade-up">{t('subpage.productsInformation.text1')}</h3>
+                    <h3 className="information_text" data-aos="fade-up">{t('subpage.productsInformation.text2')}</h3>
                     <ul className="information_list" data-aos="fade-up">
-                        <li>Szybka dostawa Katalog sprężyn i standardowych części</li>
-                        <li>Dostawa prototypów</li>
-                        <li>Stopniowe ceny sprężyn standardowych</li>
-                        <li>Najwyższa jakość</li>
-                        <li>Guaranteed quality delivery according DIN / NEN / ISO0</li>
-
+                        <li>{t('subpage.productsInformation.list1')}</li>
+                        <li>{t('subpage.productsInformation.list2')}</li>
+                        <li>{t('subpage.productsInformation.list3')}</li>
+                        <li>{t('subpage.productsInformation.list4')}</li>
+                        <li>{t('subpage.productsInformation.list5')}</li>
                     </ul>
                 </div>
             </div>
